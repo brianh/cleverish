9a4bcc7b9c0ba8839b2bf67f247c9204d89df383
bhalbert <bhalbert@psl.nmsu.edu>
2011-02-11 16:11:31 -0700

    Next....

---
+++ src/main/clojure/cleverish/adaptive_random_search.clj
@@ -0,0 +1,11 @@
+(ns cleverish.adaptive-random-search)
+
+
+
+;;
+;; Non-code stuff for easy REPL loading...
+;;
+(comment 
+  (load-file "/home/brian/code/clj/cleverish/src/main/clojure/cleverish/adaptive-random-search.clj")
+  (in-ns 'cleverish.adaptive-random-search)
+  )
