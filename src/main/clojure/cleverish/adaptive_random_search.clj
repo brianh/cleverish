2b68d23696e68f9617ad91036917c745f389651f
bhalbert <bhalbert@psl.nmsu.edu>
2011-02-16 15:48:01 -0700

    Heinous attempt at following the book code as closely as I can.  I really should have the soln be a struct or map....

--- src/main/clojure/cleverish/adaptive_random_search.clj
+++ src/main/clojure/cleverish/adaptive_random_search.clj
@@ -1,5 +1,4 @@
 (ns cleverish.adaptive-random-search
-  (:use cleverish.random-search2)
   (:use cleverish.utils))
 
 ;;;;
@@ -15,22 +14,69 @@
   "Produce a random number between min & max."
   (+ min (* (- max min) (rand))))
 
-(defn rand-vec [space-bounds]
-	(into [] (apply map #(rand-in-bounds %1 %2) space-bounds)))
+(defn rand-vec [soln-bounds]
+  (into [] (apply map #(rand-in-bounds %1 %2) soln-bounds)))
 
 ;; hmm this needs cleanup
-(defn take-step [space-bounds cur-loc step-size]
-	    (into [] (map #(let [[d-min d-max] %1
-			    new-min (max d-min (- %2 step-size))
-			    new-max (min d-max (+ %2 step-size))]
-			    (rand-in-bounds new-min new-max)) space-bounds cur-loc)))
+(defn take-step [soln-bounds cur-soln step-size]
+  (into [] (map #(let [[d-min d-max] %1
+		       new-min (max d-min (- %2 step-size))
+		       new-max (min d-max (+ %2 step-size))]
+		   (rand-in-bounds new-min new-max)) soln-bounds cur-soln)))
 
 ;; yes, java lovers, clojure treats commas as whitespace & ignores them
-(defn large-step-size [iter, step_size, s_factor, l_factor, iter_mult]
-  (if (and (pos? iter) (zero? (mod iter iter_mult)))
-    (* step_size l_factor)
-    (* step_size s_factor)))
+(defn large-step-size [iter, step-size, s-factor, l-factor, iter-mult]
+  (if (and (pos? iter) (zero? (mod iter iter-mult)))
+    (* step-size l-factor)
+    (* step-size s-factor)))
     
+(defn take-steps [soln-bounds cur-soln step-size big-step-size]
+  "Returns a vector of two new steps: [step big-step]"
+  [(take-step soln-bounds cur-soln step-size)
+   (take-step soln-bounds cur-soln big-step-size)])
+
+;; this is Fugly!  I feel dirty...
+(defn recursive-search [max-iter soln-bounds init-factor s-factor l-factor iter-mult max-no-impr cost-fn]
+  (loop [iter 0
+	 no-impr-cnt 0
+	 step-size (* (- (second (first soln-bounds)) (first (first soln-bounds))) init-factor)
+	 cur-soln (rand-vec soln-bounds)]
+    (let [big-step-size (large-step-size iter step-size s-factor l-factor iter-mult)
+	  [new-step new-large-step] (take-steps soln-bounds cur-soln step-size big-step-size)]
+      (if (> iter max-iter)
+	cur-soln
+	(let [is-better (or (<= (cost-fn new-step) (cost-fn cur-soln))
+			    (<= (cost-fn new-large-step) (cost-fn cur-soln)))
+	      [count new-step-size new-soln] (if is-better
+					       (if (<= (cost-fn new-large-step) (cost-fn new-step))
+						 [0 big-step-size new-large-step]
+						 [0 step-size new-step])
+					       (if (>= no-impr-cnt max-no-impr)
+						 [0 (/ step-size s-factor) cur-soln]
+						 [(inc no-impr-cnt) step-size cur-soln]))]
+	  (recur (inc iter) count new-step-size new-soln))))))
+
+;; not much better
+(defn recursive-search2 [max-iter soln-bounds init-factor s-factor l-factor iter-mult max-no-impr cost-fn]
+  (loop [iter 0
+	 no-impr-cnt 0
+	 step-size (* (- (second (first soln-bounds)) (first (first soln-bounds))) init-factor)
+	 cur-soln (rand-vec soln-bounds)]
+    (let [big-step-size (large-step-size iter step-size s-factor l-factor iter-mult)
+	  [new-step new-large-step] (take-steps soln-bounds cur-soln step-size big-step-size)]
+      (if (> iter max-iter)
+	cur-soln
+	(if (or (<= (cost-fn new-step) (cost-fn cur-soln))
+		(<= (cost-fn new-large-step) (cost-fn cur-soln)))
+	  (if (<= (cost-fn new-large-step) (cost-fn new-step))
+	    (recur (inc iter) 0 big-step-size new-large-step)
+	    (recur (inc iter) 0 step-size cur-soln))
+	  (recur (inc iter) (if (>= no-impr-cnt max-no-impr) 0 (inc no-impr-cnt))
+		 (if (>= no-impr-cnt max-no-impr) (/ step-size s-factor) step-size) cur-soln))))))
+
+
+
+;   (recursive-search 1000 [[-5 5] [-5 5]] 0.05 small-factor large-factor iter-mult max-no-improvement sum-squares))
 
 ;;
 ;; Non-code stuff for easy REPL loading...
