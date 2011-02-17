fe3de901bb77cb02fccb6adfb6425ac2ad0432ab
bhalbert <bhalbert@psl.nmsu.edu>
2011-02-17 16:05:24 -0700

    Preparing for another attempt.

---
+++ src/main/clojure/cleverish/adaptive_random_search2.clj
@@ -0,0 +1,16 @@
+(ns cleverish.adaptive-random-search2
+  (:use cleverish.utils))
+
+;;;;
+;;;; Another way...
+;;;;
+
+;; TBD
+
+;;
+;; Non-code stuff for easy REPL loading...
+;;
+(comment 
+  (load-file "/home/brian/code/clj/cleverish/src/main/clojure/cleverish/adaptive_random_search2.clj")
+  (in-ns 'cleverish.adaptive-random-search2)
+  )
