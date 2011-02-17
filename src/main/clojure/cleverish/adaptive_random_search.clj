903e1195e14adafe2dcdd36294bd5e33b637d12d
bhalbert <bhalbert@psl.nmsu.edu>
2011-02-17 08:14:08 -0700

    Minor cleanup.
    * Added soln as map.
    * Removed ugly single recur fn.

--- src/main/clojure/cleverish/adaptive_random_search.clj
+++ src/main/clojure/cleverish/adaptive_random_search.clj
@@ -17,6 +17,9 @@
 (defn rand-vec [soln-bounds]
   (into [] (apply map #(rand-in-bounds %1 %2) soln-bounds)))
 
+(defn make-soln [v]
+  {:vec v :cost (sum-squares v)})
+
 ;; hmm this needs cleanup
 (defn take-step [soln-bounds cur-soln step-size]
   (into [] (map #(let [[d-min d-max] %1
@@ -29,54 +32,33 @@
   (if (and (pos? iter) (zero? (mod iter iter-mult)))
     (* step-size l-factor)
     (* step-size s-factor)))
-    
+
 (defn take-steps [soln-bounds cur-soln step-size big-step-size]
   "Returns a vector of two new steps: [step big-step]"
-  [(take-step soln-bounds cur-soln step-size)
-   (take-step soln-bounds cur-soln big-step-size)])
-
-;; this is Fugly!  I feel dirty...
-(defn recursive-search [max-iter soln-bounds init-factor s-factor l-factor iter-mult max-no-impr cost-fn]
-  (loop [iter 0
-	 no-impr-cnt 0
-	 step-size (* (- (second (first soln-bounds)) (first (first soln-bounds))) init-factor)
-	 cur-soln (rand-vec soln-bounds)]
-    (let [big-step-size (large-step-size iter step-size s-factor l-factor iter-mult)
-	  [new-step new-large-step] (take-steps soln-bounds cur-soln step-size big-step-size)]
-      (if (> iter max-iter)
-	cur-soln
-	(let [is-better (or (<= (cost-fn new-step) (cost-fn cur-soln))
-			    (<= (cost-fn new-large-step) (cost-fn cur-soln)))
-	      [count new-step-size new-soln] (if is-better
-					       (if (<= (cost-fn new-large-step) (cost-fn new-step))
-						 [0 big-step-size new-large-step]
-						 [0 step-size new-step])
-					       (if (>= no-impr-cnt max-no-impr)
-						 [0 (/ step-size s-factor) cur-soln]
-						 [(inc no-impr-cnt) step-size cur-soln]))]
-	  (recur (inc iter) count new-step-size new-soln))))))
-
+  (let [step (take-step soln-bounds (:vec cur-soln) step-size)
+	big-step (take-step soln-bounds (:vec cur-soln) big-step-size)]
+    [(make-soln step) (make-soln big-step)]))
+  
 ;; not much better
-(defn recursive-search2 [max-iter soln-bounds init-factor s-factor l-factor iter-mult max-no-impr cost-fn]
+(defn recursive-search [max-iter soln-bounds init-factor s-factor l-factor iter-mult max-no-impr]
   (loop [iter 0
 	 no-impr-cnt 0
 	 step-size (* (- (second (first soln-bounds)) (first (first soln-bounds))) init-factor)
-	 cur-soln (rand-vec soln-bounds)]
+	 cur-soln (make-soln (rand-vec soln-bounds))]
     (let [big-step-size (large-step-size iter step-size s-factor l-factor iter-mult)
 	  [new-step new-large-step] (take-steps soln-bounds cur-soln step-size big-step-size)]
       (if (> iter max-iter)
 	cur-soln
-	(if (or (<= (cost-fn new-step) (cost-fn cur-soln))
-		(<= (cost-fn new-large-step) (cost-fn cur-soln)))
-	  (if (<= (cost-fn new-large-step) (cost-fn new-step))
+	(if (or (<= (:cost new-step) (:cost cur-soln))
+		(<= (:cost new-large-step) (:cost cur-soln)))
+	  (if (<= (:cost new-large-step) (:cost new-step))
 	    (recur (inc iter) 0 big-step-size new-large-step)
 	    (recur (inc iter) 0 step-size cur-soln))
 	  (recur (inc iter) (if (>= no-impr-cnt max-no-impr) 0 (inc no-impr-cnt))
 		 (if (>= no-impr-cnt max-no-impr) (/ step-size s-factor) step-size) cur-soln))))))
 
 
-
-;   (recursive-search 1000 [[-5 5] [-5 5]] 0.05 small-factor large-factor iter-mult max-no-improvement sum-squares))
+;   (recursive-search 1000 [[-5 5] [-5 5]] 0.05 small-factor large-factor iter-mult max-no-improvement))
 
 ;;
 ;; Non-code stuff for easy REPL loading...
